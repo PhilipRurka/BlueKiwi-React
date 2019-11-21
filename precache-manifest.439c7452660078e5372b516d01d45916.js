3d2ceaefebf23e4756ffea5e1407bd4f0c77e854
Philip Rurka <philiprurka@gmail.com>
2019-11-21 15:12:43 -0500

    Updates

---
+++ precache-manifest.439c7452660078e5372b516d01d45916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd483624b2e832264ec4cda524e7b91",
+    "url": "/BlueKiwi-React/index.html"
+  },
+  {
+    "revision": "ea26af6bf1db3a9e0a97",
+    "url": "/BlueKiwi-React/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ace3fb99bb320aa30a94",
+    "url": "/BlueKiwi-React/static/js/2.27e8415e.chunk.js"
+  },
+  {
+    "revision": "ea26af6bf1db3a9e0a97",
+    "url": "/BlueKiwi-React/static/js/main.0ea51e00.chunk.js"
+  },
+  {
+    "revision": "2e61192823063905d581",
+    "url": "/BlueKiwi-React/static/js/runtime-main.f179ddec.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/BlueKiwi-React/static/media/logo.25bf045c.svg"
+  }
+]);
