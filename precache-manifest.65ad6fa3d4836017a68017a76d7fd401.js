a8f7b7c309990b382c9cc42e63384c51bd8edb89
Philip Rurka <philiprurka@gmail.com>
2019-11-22 16:16:40 -0500

    Updates

--- precache-manifest.65ad6fa3d4836017a68017a76d7fd401.js
+++ precache-manifest.65ad6fa3d4836017a68017a76d7fd401.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2968fa2e5817d5c905716f6605dfc885",
+    "revision": "f3a98149970dce460ae2f9dc4b0e317e",
     "url": "/BlueKiwi-React/index.html"
   },
   {
-    "revision": "dba5821b5f781175d147",
+    "revision": "ff1364434c1a189ba5e8",
     "url": "/BlueKiwi-React/static/css/main.e7daed52.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/BlueKiwi-React/static/js/2.9e4284fb.chunk.js"
   },
   {
-    "revision": "dba5821b5f781175d147",
-    "url": "/BlueKiwi-React/static/js/main.e115806d.chunk.js"
+    "revision": "ff1364434c1a189ba5e8",
+    "url": "/BlueKiwi-React/static/js/main.e309d439.chunk.js"
   },
   {
     "revision": "2e61192823063905d581",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BlueKiwi-React/precache-manifest.f1bcdfff462220b1d5cdd71c5c460ff5.js"
+  "/BlueKiwi-React/precache-manifest.65ad6fa3d4836017a68017a76d7fd401.js"
 );
 
 self.addEventListener('message', (event) => {
