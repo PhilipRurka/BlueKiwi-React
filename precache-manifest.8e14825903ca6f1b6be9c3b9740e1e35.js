38c8d67c7a9a8a9918dac2e6e608f8a44ed2399f
Philip Rurka <philiprurka@gmail.com>
2019-11-27 16:58:05 -0500

    Updates

--- precache-manifest.8e14825903ca6f1b6be9c3b9740e1e35.js
+++ precache-manifest.8e14825903ca6f1b6be9c3b9740e1e35.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c228a7b7ac6b8a70c9a3fcb4097b689d",
+    "revision": "92bc0ae1c447fcfc974d20d7c1399ad1",
     "url": "/BlueKiwi-React/index.html"
   },
   {
-    "revision": "e4a9a9991152d91e4ce7",
+    "revision": "cf171c1a72ffd198405e",
     "url": "/BlueKiwi-React/static/css/main.e7daed52.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/BlueKiwi-React/static/js/2.9753ea57.chunk.js"
   },
   {
-    "revision": "e4a9a9991152d91e4ce7",
-    "url": "/BlueKiwi-React/static/js/main.71e80b37.chunk.js"
+    "revision": "cf171c1a72ffd198405e",
+    "url": "/BlueKiwi-React/static/js/main.ab9e623a.chunk.js"
   },
   {
     "revision": "2e61192823063905d581",
